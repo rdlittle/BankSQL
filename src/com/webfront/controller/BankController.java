e7c02632e1cade8d7d696c7aa818ba8c40d62fd8
Bob <Bob@CAMARO.littleacres.lan>
2017-01-12 19:59:35 -0500

    Added controls to Report

--- src/com/webfront/controller/BankController.java
+++ src/com/webfront/controller/BankController.java
@@ -341,7 +341,6 @@ public class BankController implements Initializable {
                             break;
                         }
                     }
-
                 }
             }
         }
